d3112b1b27638d2d59ebeeb55d2e5820312ea1bd
ksyoo <nextree8@partner.samsung.com>
2022-11-25 16:00:27 +0900

    add comments

--- src/grid/GridExample.tsx
+++ src/grid/GridExample.tsx
@@ -8,11 +8,11 @@ const GridExample = () => {
 
     const ref = useRef(null);
     const [ loading, setLoading ] = useState(false);
-
+    // FIXME : 초기 렌더링 시 여기서 한번, useFetch 내에서 (fetchData()) 한번으로 총 2번의 중복 쿼리 발생 (useFetch.ts[15:30])
     useEffect(() => {
         initData();
     }, []);
-
+    // FIXME : 맨 처음 호출될 때 두번 불리는 것 같은데 이 부분은 왜그런 걸까요?
     const handleObserver = useCallback((entries: any) => {
         const target = entries[0];
         console.log(target);
--- src/hook/useFetch.ts
+++ src/hook/useFetch.ts
@@ -1,7 +1,7 @@
 import {useCallback, useEffect, useState} from "react";
 import {TodoType} from "../agGrid/TodoType";
 import Apis from "../Apis";
-
+// FIXME TodoType 뿐만 아니라 다양한 모델에 재사용 가능하도록 하는 방안을 고민해보면 좋을 것 같아요. (generic + fetcher 함수를 인자로 받기 등)
 const useFetch = ():[TodoType[], boolean, () => void, () => void] => {
 
     const apis = Apis.instance;
@@ -14,7 +14,7 @@ const useFetch = ():[TodoType[], boolean, () => void, () => void] => {
     useEffect(() => {
         !stop && fetchData();
     },[offset]);
-
+    // FIXME initData와 중복코드,
     const fetchData = useCallback(() => {
         apis.getDataList(offset, limit).then(res => {
             if( res.data.length < limit ) setStop(true);
